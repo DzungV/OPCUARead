66ad2997ec0bb6abfe408f1789a74b83560143e8
ADMIN <ADMIN@LAPTOP-IT96R3EJ>
2022-07-07 10:59:41 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace OPCUARead
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOpcValue = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatusMessage = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.lbStt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature";
+            // 
+            // txtOpcValue
+            // 
+            this.txtOpcValue.Location = new System.Drawing.Point(115, 385);
+            this.txtOpcValue.Name = "txtOpcValue";
+            this.txtOpcValue.Size = new System.Drawing.Size(310, 27);
+            this.txtOpcValue.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(688, 43);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 29);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(688, 102);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 29);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatusMessage
+            // 
+            this.lblStatusMessage.AutoSize = true;
+            this.lblStatusMessage.Location = new System.Drawing.Point(142, 29);
+            this.lblStatusMessage.Name = "lblStatusMessage";
+            this.lblStatusMessage.Size = new System.Drawing.Size(107, 20);
+            this.lblStatusMessage.TabIndex = 3;
+            this.lblStatusMessage.Text = "StatusMessage";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Position";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(142, 99);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 27);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(142, 162);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(125, 27);
+            this.txtStatus.TabIndex = 5;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(142, 224);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(125, 27);
+            this.txtPosition.TabIndex = 5;
+            // 
+            // lbStt
+            // 
+            this.lbStt.AutoSize = true;
+            this.lbStt.Location = new System.Drawing.Point(30, 316);
+            this.lbStt.Name = "lbStt";
+            this.lbStt.Size = new System.Drawing.Size(60, 20);
+            this.lbStt.TabIndex = 4;
+            this.lbStt.Text = "IsActive";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 586);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbStt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStatusMessage);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOpcValue);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtOpcValue;
+        private Button btnStart;
+        private Button btnStop;
+        private Label lblStatusMessage;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtName;
+        private TextBox txtStatus;
+        private TextBox txtPosition;
+        private Label lbStt;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+using Opc.UaFx.Client;
+
+namespace OPCUARead
+{
+    public partial class Form1 : Form
+    {
+        OpcClient client = new OpcClient("opc.tcp://localhost:4840/");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 100;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            client.Connect();
+            timer1.Start();
+            lblStatusMessage.Text = "Connected to OPC Server";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (client != null)
+                client.Disconnect();
+            lblStatusMessage.Text = "Disconected from OPC Server";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (client != null)
+                client.Disconnect();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            OpcRead();
+        }
+
+        void OpcRead()
+        {
+            //temperature
+            string tagName = "ns=2;s=Temperature";
+            Opc.UaFx.OpcValue opcTemp = client.ReadNode(tagName);
+
+            double temperature = (double)opcTemp.Value;
+            txtOpcValue.Text = temperature.ToString("#.##");
+
+            // name
+            tagName = "ns=2;s=Name";
+            Opc.UaFx.OpcValue opcName = client.ReadNode(tagName);
+
+            string Name = (string)opcName.Value;
+            txtName.Text = Name.ToString();
+
+            // position
+            tagName = "ns=2;s=Position";
+            Opc.UaFx.OpcValue opcPos = client.ReadNode(tagName);
+
+            sbyte position = (sbyte)opcPos.Value;
+            txtPosition.Text = position.ToString();
+
+            // IsActive
+            tagName = "ns=2;s=IsActive";
+            Opc.UaFx.OpcValue opcIsActive = client.ReadNode(tagName);
+
+            bool stt = (bool)opcIsActive.Value;
+            lbStt.Text = stt.ToString();
+
+            // status
+            tagName = "ns=2;s=Status";
+            Opc.UaFx.OpcValue opcStt = client.ReadNode(tagName);
+
+            byte STT = (byte)opcStt.Value;
+            txtStatus.Text = STT.ToString();
+
+            //string tagName = "ns=2;s=Temperature";
+            //Opc.UaFx.OpcValue opcTemp = client.ReadNode(tagName);
+
+            //double temperature = (double)opcTemp.Value;
+            //txtOpcValue.Text = temperature.ToString();
+
+
+
+
+        }
+    }
+}
